e8f72a7be8186f2f7d1de2b9151c7e304245a141
Anh Thu <thuthuanh19@gmail.com>
2023-03-29 10:01:00 +0700

    update search form weather forecast

--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -3,13 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Chart from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartColumn } from '@fortawesome/free-solid-svg-icons';
-import { Calendar, theme } from "antd";
+import { faChartColumn, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { getAllUser } from "../../utils/getUser";
-import {changeFormatDate, changeFormatDate1} from "../../utils/formatDate";
+import { changeFormatDate, changeFormatDate1 } from "../../utils/formatDate";
 import { getAllLoginTimes } from "../../utils/loginTimes";
 import "./style.css";
-import {fetchDataWeather, showIcon} from "../../utils/weather";
+import { fetchDataWeather, showIcon } from "../../utils/weather";
 
 function DashBoard() {
     const userToken = localStorage.getItem("token");
@@ -18,43 +17,32 @@ function DashBoard() {
     const [recentAddedUser, setRecentAddedUser] = useState();
     const [recentUpdatedUser, setRecentUpdatedUser] = useState();
 
-    //Using antd calendar
-    // const onPanelChange = (value, mode) => {
-    //     console.log(value.format('YYYY-MM-DD'), mode);
-    // };
-    // const { token } = theme.useToken();
-    // const wrapperStyle = {
-    //     width: 300,
-    //     border: `1px solid ${token.colorBorderSecondary}`,
-    //     borderRadius: token.borderRadiusLG,
-    // };
-
     //Function to get recently user added/updated
     const getRecentlyUser = (propertyFilter, users, todayTime) => {
         let arr = users.filter(person => {
-            const time = new Date(person[propertyFilter]); 
+            const time = new Date(person[propertyFilter]);
             return time.getDate() === todayTime.getDate() && time.getMonth() === todayTime.getMonth() && time.getFullYear() === todayTime.getFullYear();
         });
         //Sort property desc
-        arr = arr.sort((a, b) => { return new Date(b[propertyFilter]) - new Date(a[propertyFilter])});
+        arr = arr.sort((a, b) => { return new Date(b[propertyFilter]) - new Date(a[propertyFilter]) });
 
         //Take 3 top users
         const newArr = [];
-        for(let i = 0; i<3; i++){
+        for (let i = 0; i < 3; i++) {
             newArr.push(arr[i]);
         }
         return newArr;
     }
 
     //Function to get username of top active users
-    const getUsername = async(arr) => {
+    const getUsername = async (arr) => {
         const data = await getAllUser(userToken);
         const username = [];
         arr.map((item) => {
             data.map(user => {
-            if(item.userId == user._id){
-                username.push(user.username);
-            }
+                if (item.userId == user._id) {
+                    username.push(user.username);
+                }
             })
         })
         return username;
@@ -64,22 +52,29 @@ function DashBoard() {
     const [weather, setWeather] = useState();
     const [currentIcon, setCurrentIcon] = useState();
     const [displayIcon, setDisplayIcon] = useState([]);
+    const [keyWord, setKeyWord] = useState("Ho Chi Minh");
+    const [isError, setIsError] = useState(false);
+    const [errorMess, setErrorMess] = useState("");
 
     //Fetch data weather
     const fetchWeather = async () => {
-        const json = await fetchDataWeather();
+        const json = await fetchDataWeather(keyWord);
         if (json) {
-            console.log("weather",json);
-          setWeather(json);
-          setCurrentIcon(showIcon(json.current.condition.text));
-          let newArr = [];
-          json.forecast.forecastday.map((item) => {
-            let source = showIcon(item.day.condition.text);
-            newArr.push(source);
-          });
-          setDisplayIcon(newArr);
+            console.log("weather", json);
+            setWeather(json);
+            setCurrentIcon(showIcon(json.current.condition.text));
+            let newArr = [];
+            json.forecast.forecastday.map((item) => {
+                let source = showIcon(item.day.condition.text);
+                newArr.push(source);
+            });
+            setDisplayIcon(newArr);
         }
-      };
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
 
     useEffect(() => {
         //Get all users
@@ -93,25 +88,25 @@ function DashBoard() {
                     //Take 3 recently added users (who was added today)
                     let recentAddedArr = getRecentlyUser("createdAt", data, today);
                     //Just take undefined user
-                    let addedArr = recentAddedArr.filter( user => user !== undefined);
+                    let addedArr = recentAddedArr.filter(user => user !== undefined);
                     setRecentAddedUser(addedArr);
 
                     //Take 3 recently updated users (who was updated today)
                     let recentUpdatedArr = getRecentlyUser("updatedAt", data, today);
                     //Just take undefined user
-                    let updatedArr = recentUpdatedArr.filter( user => user !== undefined);
+                    let updatedArr = recentUpdatedArr.filter(user => user !== undefined);
                     setRecentUpdatedUser(updatedArr);
                 })
         }
 
         //Get all login times of top 10 users for map data
-        async function getAllLoginTimesOfUsers(){
+        async function getAllLoginTimesOfUsers() {
             await getAllLoginTimes()
                 .then(async data => {
                     //Chart data
                     //Sort data based on logintime desc
                     const arr = [];
-                    
+
                     data.sort((a, b) => b.logintime - a.logintime);
 
                     //Take 10 top activity users
@@ -121,7 +116,7 @@ function DashBoard() {
 
                     //labels for chart by username
                     const username = await getUsername(arr);
-                    
+
                     let dataLoginTime = [];
                     //Take labels as username and data as logintimes for chart
                     arr.map((item) => {
@@ -145,8 +140,13 @@ function DashBoard() {
         }
         getData();
         getAllLoginTimesOfUsers();
-        fetchWeather()
-    }, [])
+        fetchWeather(keyWord).catch((error) => {
+            console.log(error);
+            setIsError(true);
+            setErrorMess("Location is not found. Please type again!!!");
+        });
+        setIsError(false)
+    }, [keyWord])
 
     return (
         <div className="p-3">
@@ -191,7 +191,7 @@ function DashBoard() {
                                     ))
                                 ) : (
                                     <>There is no student added today.</>
-                                ) 
+                                )
                             }
                         </div>
                     </div>
@@ -200,14 +200,14 @@ function DashBoard() {
                 {/* Show card of recently updated students */}
                 <div className="col-lg-4 col-sm-12 mb-3">
                     <div className="bg-white border-0 rounded-3 card-item p-3">
-                        <p className="d-flex justify-content-between align-items-center fw-bolder text-black-50">Recently Updated Students 
+                        <p className="d-flex justify-content-between align-items-center fw-bolder text-black-50">Recently Updated Students
                             <img src='https://cdn-icons-png.flaticon.com/512/5511/5511397.png' className="dashboardIcon"></img>
                         </p>
                         <hr></hr>
 
                         {/* Display 3 user recently updated today */}
                         <div>
-                        {
+                            {
                                 recentUpdatedUser && recentUpdatedUser?.length != 0 ? (
                                     recentUpdatedUser?.map((item, index) => (
                                         <div key={index} className='d-flex justify-content-between'>
@@ -224,7 +224,7 @@ function DashBoard() {
                                     ))
                                 ) : (
                                     <>There is no student updated their infomation today.</>
-                                ) 
+                                )
                             }
                         </div>
                     </div>
@@ -250,55 +250,62 @@ function DashBoard() {
                     </div>
                 </div>
                 <div className="col-sm-12 col-lg-5">
-                    {/* <div style={wrapperStyle} className='calendarBody'>
-                        <Calendar fullscreen={false} onPanelChange={onPanelChange} />
-                    </div> */}
                     <div className="weatherForcast bg-white rounded-3 p-3">
-                        <h4 style={{color: "gray"}} className="fw-bolder">{weather?.location?.name} - {weather?.location?.country}</h4>
-                        <hr></hr>
-                        <div className="row mx-2 text-white weatherItems">
-                            <div className="col text-center pt-2 rounded-4 weatherItem2 me-2">
-                                <p className="fw-bolder">{changeFormatDate1(weather?.forecast?.forecastday[0]?.date)}</p>
-                                <img src={currentIcon} className="weatherIcon"></img>
-                                <p>
-                                    {weather?.current?.temp_c}
-                                    <sup>o</sup>
-                                    <button className="rounded-circle border-0 ms-2 fw-bolder">C</button>
-                                </p>
-                                <small>
-                                    {weather?.current?.condition.text}
-                                </small>
-                            </div>
-                            <div className="col text-center pt-2 rounded-4 weatherItem me-2">
-                                <p className="fw-bolder">{changeFormatDate1(weather?.forecast?.forecastday[1]?.date)}</p>
-                                <img src={displayIcon && displayIcon[1]} className="weatherIcon"></img>
-                                <p>
-                                    {weather?.forecast?.forecastday[1]?.day?.avgtemp_c}
-                                    <sup>o</sup>
-                                    <button className="rounded-circle border-0 ms-2 fw-bolder">C</button>
-                                </p>
-                                <small>
-                                    {weather?.forecast?.forecastday[1]?.day?.condition?.text}
-                                </small>
-                            </div>
-                            <div className="col text-center pt-2 rounded-4 weatherItem">
-                                <p className="fw-bolder">{changeFormatDate1(weather?.forecast?.forecastday[2]?.date)}</p>
-                                <img src={displayIcon && displayIcon[2]} className="weatherIcon"></img>
-                                <p>
-                                    {weather?.forecast?.forecastday[2]?.day?.avgtemp_c}
-                                    <sup>o</sup>
-                                    <button className="rounded-circle border-0 ms-2 fw-bolder">C</button>
-                                </p>
-                                <small>
-                                    {weather?.forecast?.forecastday[2]?.day?.condition?.text}
-                                </small>
-                            </div>
+                        <form className="searchFormLocation ms-2 mb-4" onSubmit={handleSubmit}>
+                            <input className="border-0 fw-bolder text-black-50" type="text" name="keyword" id="keyword" value={keyWord} onChange={(e) => setKeyWord(e.target.value)} />
+                            <button type="submit" className="border-0 bg-white"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+                        </form>
+
+                        {
+                            isError == true ? (
+                                <p className="text-danger">{errorMess}</p>
+                             ) : (
+                                < div className="row mx-2 text-white weatherItems">
+                        <div className="col text-center pt-2 rounded-4 weatherItem2 me-2">
+                            <p className="fw-bolder">{changeFormatDate1(weather?.forecast?.forecastday[0]?.date)}</p>
+                            <img src={currentIcon} className="weatherIcon"></img>
+                            <p>
+                                {weather?.current?.temp_c}
+                                <sup>o</sup>
+                                <button className="rounded-circle border-0 ms-2 fw-bolder">C</button>
+                            </p>
+                            <small>
+                                {weather?.current?.condition.text}
+                            </small>
+                        </div>
+                        <div className="col text-center pt-2 rounded-4 weatherItem me-2">
+                            <p className="fw-bolder">{changeFormatDate1(weather?.forecast?.forecastday[1]?.date)}</p>
+                            <img src={displayIcon && displayIcon[1]} className="weatherIcon"></img>
+                            <p>
+                                {weather?.forecast?.forecastday[1]?.day?.avgtemp_c}
+                                <sup>o</sup>
+                                <button className="rounded-circle border-0 ms-2 fw-bolder">C</button>
+                            </p>
+                            <small>
+                                {weather?.forecast?.forecastday[1]?.day?.condition?.text}
+                            </small>
+                        </div>
+                        <div className="col text-center pt-2 rounded-4 weatherItem">
+                            <p className="fw-bolder">{changeFormatDate1(weather?.forecast?.forecastday[2]?.date)}</p>
+                            <img src={displayIcon && displayIcon[2]} className="weatherIcon"></img>
+                            <p>
+                                {weather?.forecast?.forecastday[2]?.day?.avgtemp_c}
+                                <sup>o</sup>
+                                <button className="rounded-circle border-0 ms-2 fw-bolder">C</button>
+                            </p>
+                            <small>
+                                {weather?.forecast?.forecastday[2]?.day?.condition?.text}
+                            </small>
                         </div>
                     </div>
+                             )
+                        }
+
                 </div>
             </div>
-
         </div>
+
+        </div >
     )
 }
 export default DashBoard;
--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,21 +1,23 @@
 //Show icon weather 
 const showIcon = (text) => {
-    let iconSrc = "";
-    if (text.includes("sunny") || text == "Sunny") {
-      iconSrc = "https://cdn-icons-png.flaticon.com/512/869/869869.png";
-    } else if (text.includes("rain")) {
-      iconSrc = "https://cdn-icons-png.flaticon.com/128/4246/4246656.png";
-    } else if (text.includes("drizzle")) {
-      iconSrc = "https://cdn-icons-png.flaticon.com/128/3075/3075858.png";
-    } else if (text.includes("overcast") || text == "Overcast") {
-      iconSrc = "https://cdn-icons-png.flaticon.com/128/1146/1146869.png";
-    } else if (text.includes("fog") || text == "Fog") {
-      iconSrc = "https://cdn-icons-png.flaticon.com/512/2076/2076792.png";
-    } else {
-      iconSrc = "https://cdn-icons-png.flaticon.com/128/414/414927.png";
-    }
-    return iconSrc;
-  };
+  let iconSrc = "";
+  if (text.includes("sunny") || text == "Sunny") {
+    iconSrc = "https://cdn-icons-png.flaticon.com/512/869/869869.png";
+  } else if (text.includes("rain")) {
+    iconSrc = "https://cdn-icons-png.flaticon.com/128/4246/4246656.png";
+  } else if (text.includes("snow")) {
+    iconSrc = "https://cdn-icons-png.flaticon.com/128/2942/2942909.png";
+  } else if (text.includes("drizzle")) {
+    iconSrc = "https://cdn-icons-png.flaticon.com/128/3075/3075858.png";
+  } else if (text.includes("overcast") || text == "Overcast") {
+    iconSrc = "https://cdn-icons-png.flaticon.com/128/1146/1146869.png";
+  } else if (text.includes("fog") || text == "Fog") {
+    iconSrc = "https://cdn-icons-png.flaticon.com/512/2076/2076792.png";
+  } else {
+    iconSrc = "https://cdn-icons-png.flaticon.com/128/414/414927.png";
+  }
+  return iconSrc;
+};
 
 //Fetch data weather
 const fetchDataWeather = async (q = "Ho Chi Minh") => {
